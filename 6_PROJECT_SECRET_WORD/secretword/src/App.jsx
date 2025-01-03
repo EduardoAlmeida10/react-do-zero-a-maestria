4fab6b40eb8e5cbe7a3ae7ca6d009ce5f7a19433
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-03 12:12:50 -0300

    stages, game and gameOver created

--- 6_PROJECT_SECRET_WORD/secretword/src/App.jsx
+++ 6_PROJECT_SECRET_WORD/secretword/src/App.jsx
@@ -1,12 +1,32 @@
+//Css
 import './App.css'
-
+// React
+import { useCallback, useEffect, useState } from 'react'
+// Data
+import {wordsList} from "./data/words.js"
+// Components
 import StartScreen from './components/StartScreen'
+import GameOver from './components/GameOver.jsx'
+import Game from './components/Game.jsx'
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
