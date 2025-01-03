648ae4b7b6e5846cb5cd6d65af405aba9102342a
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-03 12:33:13 -0300

    functions stages created

--- 6_PROJECT_SECRET_WORD/secretword/src/App.jsx
+++ 6_PROJECT_SECRET_WORD/secretword/src/App.jsx
@@ -3,16 +3,16 @@ import './App.css'
 // React
 import { useCallback, useEffect, useState } from 'react'
 // Data
-import {wordsList} from "./data/words.js"
+import { wordsList } from "./data/words.js"
 // Components
 import StartScreen from './components/StartScreen'
 import GameOver from './components/GameOver.jsx'
 import Game from './components/Game.jsx'
 
 const stages = [
-  {id: 1, name: "start"},
-  {id: 2, name: "game"},
-  {id: 3, name: "end"}
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
 ]
 
 function App() {
@@ -20,13 +20,26 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words);
+  // starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className='App'>
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
