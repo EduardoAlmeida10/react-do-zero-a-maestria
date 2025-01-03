286921f0083b4b06a4d6dca842cde70ba6230e85
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-03 13:21:45 -0300

    structuring game component

--- 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
+++ 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
@@ -1,12 +1,34 @@
 import "./Game.css"
 
-const Game = ({verifyLetter}) => {
-  return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalizar jogo</button>
-    </div>
-  )
+const Game = ({ verifyLetter }) => {
+    return (
+        <div className="game">
+            <p className="points">
+                <span>Pontuação: 000</span>
+            </p>
+            <h1>Adivinhe a palavra:</h1>
+            <h3 className="tip">
+                Dica sobre a palavra: <span>Dica...</span>
+            </h3>
+            <div className="wordConteiner">
+                <span className="letter">A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className="letterConteiner">
+                <p>Tente adivinhar uma letra da palavra: </p>
+                <form>
+                    <input type="text" name="letter" maxLength="1" required />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersConteiner">
+                <p>Letras já utilizadas: </p>
+                <span>a, </span>
+                <span>b, </span>
+                <span>c</span>
+            </div>
+        </div>
+    )
 }
 
 export default Game
