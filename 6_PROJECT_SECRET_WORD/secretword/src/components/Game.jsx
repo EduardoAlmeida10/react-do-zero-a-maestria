87f4e5183a41c4860b52752acee626580e818ae0
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-03 21:41:11 -0300

    consuming states in the game

--- 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
+++ 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
@@ -1,18 +1,26 @@
 import "./Game.css"
 
-const Game = ({ verifyLetter }) => {
+import PropTypes from 'prop-types';
+
+const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
     return (
         <div className="game">
             <p className="points">
-                <span>Pontuação: 000</span>
+                <span>Pontuação: {score}</span>
             </p>
             <h1>Adivinhe a palavra:</h1>
             <h3 className="tip">
-                Dica sobre a palavra: <span>Dica...</span>
+                Dica sobre a palavra: <span>{pickedCategory}</span>
             </h3>
+            <p>Você ainda tem {guesses} tentativa(s).</p>
             <div className="wordContainer">
-                <span className="letter">A</span>
-                <span className="blankSquare"></span>
+                {letters.map((letter, i) => (
+                    guessedLetters.includes(letter) ? (
+                        <span key={i} className="letter">{letter}</span>
+                    ) : (
+                        <span key={i} className="blankSquare"></span>
+                    )
+                ))}
             </div>
             <div className="letterContainer">
                 <p>Tente adivinhar uma letra da palavra: </p>
@@ -23,12 +31,23 @@ const Game = ({ verifyLetter }) => {
             </div>
             <div className="wrongLettersContainer">
                 <p>Letras já utilizadas: </p>
-                <span>a, </span>
-                <span>b, </span>
-                <span>c</span>
+                {wrongLetters.map((letter, i) =>(
+                    <span key={i}>{letter}, </span>
+                ))}
             </div>
         </div>
     )
 }
+
+Game.propTypes = {
+    verifyLetter: PropTypes.func.isRequired,
+    pickedWord: PropTypes.string.isRequired,
+    pickedCategory: PropTypes.string.isRequired,
+    letters: PropTypes.arrayOf(PropTypes.string).isRequired,
+    guessedLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
+    wrongLetters: PropTypes.arrayOf(PropTypes.string).isRequired,
+    guesses: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
+};
 
 export default Game
