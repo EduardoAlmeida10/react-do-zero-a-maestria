3f428cd647167f5164bd72ac44134b55bacd4827
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-03 21:57:23 -0300

    Letter insertion event

--- 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
+++ 6_PROJECT_SECRET_WORD/secretword/src/components/Game.jsx
@@ -1,8 +1,23 @@
+import { useState, useRef } from "react";
+
 import "./Game.css"
 
 import PropTypes from 'prop-types';
 
 const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
+
+    const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        verifyLetter(letter);
+        setLetter("");
+
+        letterInputRef.current.focus();
+    }
+
     return (
         <div className="game">
             <p className="points">
@@ -24,14 +39,22 @@ const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetter
             </div>
             <div className="letterContainer">
                 <p>Tente adivinhar uma letra da palavra: </p>
-                <form>
-                    <input type="text" name="letter" maxLength="1" required />
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="letter"
+                        maxLength="1"
+                        required
+                        onChange={(e) => setLetter(e.target.value)}
+                        value={letter}
+                        ref={letterInputRef}
+                    />
                     <button>Jogar!</button>
                 </form>
             </div>
             <div className="wrongLettersContainer">
                 <p>Letras já utilizadas: </p>
-                {wrongLetters.map((letter, i) =>(
+                {wrongLetters.map((letter, i) => (
                     <span key={i}>{letter}, </span>
                 ))}
             </div>
