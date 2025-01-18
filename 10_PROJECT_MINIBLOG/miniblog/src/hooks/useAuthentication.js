2e4d92cd7a70f4a5545c1e24baf4caa7ebd9edd6
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-01-18 18:45:04 -0300

    login finished

--- 10_PROJECT_MINIBLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_PROJECT_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -78,7 +78,37 @@ export const useAuthentication = () => {
 
     useEffect(() => {
         return () => setCancelled(true);
-    }, [])
+    }, []);
+
+    // login
+    const login = async (data) => {
+
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+
+            setLoading(false);
+
+        } catch (error) {
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado";
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+            }
+
+            setLoading(false);
+            setError(systemErrorMessage);
+        }
+
+    }
 
     return {
         auth,
@@ -86,5 +116,6 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     };
 }
