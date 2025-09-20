2d2b288d667004ab801f73ec0c597b04a1e6b83d
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-09-20 18:09:05 -0300

    feat: created navbar and footer

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
+import NavBar from "./components/NavBar/NavBar"
+import Footer from "./components/Footer/Footer"
 
 
 function App() {
@@ -11,12 +13,14 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+      <NavBar />
         <div className="conteiner">
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   )
