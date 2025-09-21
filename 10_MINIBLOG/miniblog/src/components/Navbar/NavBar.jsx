b8eaf60aa1a6e4e8b918c45b85b65aff6b2fd1c0
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-09-21 17:15:17 -0300

    feat: blocking pages with authentication

--- 10_MINIBLOG/miniblog/src/components/Navbar/NavBar.jsx
+++ 10_MINIBLOG/miniblog/src/components/Navbar/NavBar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom"
-import { items } from "./items"
 
 import { useAuthentication } from "../../hooks/useAuthentication"
 
--- 10_MINIBLOG/miniblog/src/components/Navbar/items.js
+++
@@ -1,6 +0,0 @@
-export const items = [
-    {id:1, link: "/", name: "Home"},
-    {id:2, link: "/login", name: "Entrar"},
-    {id:3, link: "/register", name: "Cadastrar"},
-    {id:4, link: "/about", name: "Sobre"}
-];
