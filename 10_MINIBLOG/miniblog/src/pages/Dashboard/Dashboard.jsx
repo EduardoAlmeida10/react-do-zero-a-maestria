fc7d4fc9566853beaeb9bd51903266eb8d6c5f60
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-09-25 11:22:21 -0300

    feat: delete post

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 
 import { useAuthValue } from "../../context/AuthContext"
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+import { useDeleteDocument } from "../../hooks/useDeleteDocument"
 
 function Dashboard() {
 
@@ -11,10 +12,7 @@ function Dashboard() {
     const uid = user.uid
 
     const { documents: posts, loading } = useFetchDocuments("post", null, uid)
-
-    const deleteDocument = (id) => {
-
-    }
+    const {deleteDocument} = useDeleteDocument("post")
 
     if(loading){
         return <p>Carregando...</p>
